c8b2cf9faf9c1083ebfceda3b8a24fd62a35f7d4
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 01:22:16 -0500

    Number 2--Using number 1 as a base

--- lp.pl
+++ lp.pl
@@ -1,7 +1,22 @@
-%Number1
-sum-up-numbers-simple([], 0).
+/*Number 1*/
+sum-up-numbers-simple([], 0). 
+    /* */
     sum-up-numbers-simple([H|T], N) :-
     number(H), sum-up-numbers-simple(T, Total),
+    /*N=H+Total*/
     N is H + Total.
+    /*Recursion...adding it to the previous*/
     sum-up-numbers-simple([H|T], N):-
         \+number(H), sum-up-numbers-simple(T, N).
+
+/*Number 2*/
+sum-up-numbers-simple([], 0). 
+    /* */
+    sum-up-numbers-simple([H|T], N) :-
+    number(H), sum-up-numbers-simple(T, Total),
+    /*N=H+Total*/
+    N is H + Total.
+    /*Recursion...adding it to the previous*/
+    sum-up-numbers-simple([H|T], N):-
+        \+number(H), sum-up-numbers-simple(T, N).
+
