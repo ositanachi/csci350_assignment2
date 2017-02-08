6fe1bdeb64c7b363b3adc1e72c4cb792dcdf535e
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 01:13:47 -0500

    Formatting and logic

--- lp.pl
+++ lp.pl
@@ -1,6 +1,7 @@
+%Number1
 sum-up-numbers-simple([], 0).
-sum-up-numbers-simple([H|T], N) :- 
-    number(H), sum-up-numbers-simple(T, total),
+    sum-up-numbers-simple([H|T], N) :-
+    number(H), sum-up-numbers-simple(T, Total),
     N is H + Total.
-    sum-up-numbers([H|T], N) :-
-        \ + number(H), sum-up-numbers-simple(T, N).
+    sum-up-numbers-simple([H|T], N):-
+        \+number(H), sum-up-numbers-simple(T, N).
