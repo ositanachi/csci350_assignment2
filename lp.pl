c4dcfebf51157c26d2442ead993e8efc065a4df2
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 05:58:17 -0500

    Number 3

--- lp.pl
+++ lp.pl
@@ -12,17 +12,23 @@ sum-up-numbers-simple([], 0).
 /*Number 2*/
 sum-up-numbers-general([], 0). 
 sum-up-numbers-general([[]], 0). /*For nested lists*/
-sum-up-numbers-general([H|T], N) :-
-    number(H), 
-    sum-up-numbers-general(T, Total),
-    /*N=H+Total*/
-    N is H + Total. /*Recursion...adding it to the previous*/
-    /*Need to account for letters*/
     sum-up-numbers-general([H|T], N) :-
-        atom(H),
-        /*If it is a letter, ignore and continue with tail*/
-        sum-up-numbers-general(T, N).
-    sum-up-numbers-general([H|T], N) :-
-        \+number(H),
-        /*Add number and continue with next item in list*/
-        sum-up-numbers-general(T, N).
+        number(H), 
+        sum-up-numbers-general(T, Total),
+        /*N=H+Total*/
+        N is H + Total. /*Recursion...adding it to the previous*/
+        /*Need to account for letters*/
+        sum-up-numbers-general([H|T], N) :-
+            atom(H),
+            /*If it is a letter, ignore and continue with tail*/
+            sum-up-numbers-general(T, N).
+        sum-up-numbers-general([H|T], N) :-
+            \+number(H),
+            /*Add number and continue with next item in list*/
+            sum-up-numbers-general(T, N).
+
+/*Number 3*/
+min-above-min(L1, L2, N).
+
+/*Number 4*/
+
