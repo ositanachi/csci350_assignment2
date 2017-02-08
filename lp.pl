bffb3066dc45b43f5e5f1682f9409550321faa9d
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 01:05:51 -0500

    Forgot recurrsive part, adding it in now

--- lp.pl
+++ lp.pl
@@ -2,4 +2,5 @@ sum-up-numbers-simple([], 0).
 sum-up-numbers-simple([H|T], N) :- 
     number(H), sum-up-numbers-simple(T, total),
     N is H + Total.
-
+    sum-up-numbers([H|T], N) :-
+        \ + number(H), sum-up-numbers-simple(T, N).
