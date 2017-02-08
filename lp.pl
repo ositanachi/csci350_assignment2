26f6e0c88db74bb15c15d4bbadce39ae8533bf9c
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 01:51:16 -0500

    Number 2: Accounting for letters (both nested and whatnot)

--- lp.pl
+++ lp.pl
@@ -13,10 +13,14 @@ sum-up-numbers-simple([], 0).
 sum-up-numbers-simple([], 0). 
 sum-up-numbers-general([[]], 0). /*For nested lists*/
 sum-up-numbers-simple([H|T], N) :-
-    number(H), sum-up-numbers-simple(T, Total),
+    number(H), 
+    sum-up-numbers-simple(T, Total),
     /*N=H+Total*/
-    N is H + Total.
-    /*Recursion...adding it to the previous*/
-    sum-up-numbers-simple([H|T], N):-
-        \+number(H), sum-up-numbers-simple(T, N).
+    N is H + Total. /*Recursion...adding it to the previous*/
+    /*Need to account for letters*/
+    sum-up-numbers-general([H|T], N) :-
+        atom(H).
+    sum-up-numbers-simple([H|T], N) :-
+        \+number(H), 
+        sum-up-numbers-simple(T, N).
 
