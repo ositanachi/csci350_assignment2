8ca9426c72b657b7abb3171e81b45b0d6e64689d
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 01:34:27 -0500

    Number 2: Added statement for nested lists--crucial

--- lp.pl
+++ lp.pl
@@ -11,8 +11,8 @@ sum-up-numbers-simple([], 0).
 
 /*Number 2*/
 sum-up-numbers-simple([], 0). 
-    /* */
-    sum-up-numbers-simple([H|T], N) :-
+sum-up-numbers-general([[]], 0). /*For nested lists*/
+sum-up-numbers-simple([H|T], N) :-
     number(H), sum-up-numbers-simple(T, Total),
     /*N=H+Total*/
     N is H + Total.
