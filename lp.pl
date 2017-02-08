17aba9aeb4da33fe17015e6ea47660524813bd84
Osi Otugo <ositanachi.otugo@bison.howard.edu>
2017-02-08 09:39:42 -0500

    Number 4
    Revised code, last case seems to fail

--- lp.pl
+++ lp.pl
@@ -44,10 +44,14 @@ min-above-min(L1, L2, N).
 
 /*Number 4*/
 /*Var or List 1, 2, or 3*/
-common-unique-elements([H|L1], L2, [H|L3]) :-
-    /*True if the Head of L1 is in L2*/
+common-unique-elements(L1, L2, [H|L3]) :-
+    /*True if the Head of L3 appears in both L1 and L2*/
+    member(L1, H),
     member(L2, H),
     /*Recursive call*/
     common-unique_elements(L1, L2, L3).
-/*Account for cases where N is an empty list so it shoudl always be true*/
-common-unique-elements([], _, []).
+/*Account for cases where N is an empty list so it should always be true*/
+common-unique-elements(_, _, []).
+member([H|_], H).
+member([_|T], H) :-
+    member(T, H).
